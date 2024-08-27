007773f1a2903cdd789a21121fc829f7a080bd8d
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-08-27 23:24:39 +0200

    first commit of kafka app

---
+++ src/kafka_app/main.py
@@ -0,0 +1,94 @@
+import logging
+
+from kafka.admin import NewTopic
+from kafka.errors import TopicAlreadyExistsError
+import os
+
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+from kafka_app.core import KafkaClient
+import typer
+
+import uuid
+
+app = typer.Typer()
+
+logger = logging.getLogger(__name__)
+
+process_id = os.urandom(15).hex()
+
+@app.command()
+def producer(
+    topic_name: str,
+    num_messages: int,
+    username: str = "user", password: str = "password",
+    bootstrap_server: str = "localhost:9092",
+    cafile_path: str | None = None,
+):
+    client = KafkaClient(
+        servers=bootstrap_server.split(","),
+        username=username,
+        password=password,
+        cafile_path=cafile_path
+    )
+
+    topic_config = NewTopic(
+        name=topic_name,
+        num_partitions=1, replication_factor=3
+    )
+
+    logger.info(f"Creating new topic - {topic_name}")
+
+    try:
+        client.create_topic(topic=topic_config)
+    except TopicAlreadyExistsError:
+        logger.info(f"Topic {topic_name} already exists")
+
+    logger.info("Producer - Starting...")
+    for ith in range(num_messages):
+        message = f"{process_id}-{ith}"
+        client.produce_message(
+            topic_name=topic_name, message_content=message.encode("utf-8")
+        )
+
+@app.command()
+def consumer(
+    topic_name: str, consumer_group: str = "cg",
+    username: str = "user", password: str = "password",
+    bootstrap_server: str = "localhost:9092",
+    cafile_path: str | None = None,
+    mongo_uri: str | None = None
+):
+    client = KafkaClient(
+        servers=bootstrap_server.split(","),
+        username=username,
+        password=password,
+        cafile_path=cafile_path
+    )
+
+    if mongo_uri:
+        mongo_client = MongoClient(mongo_uri)
+        collection = mongo_client[topic_name].get_collection("consumer")
+    else:
+        collection = None
+
+    logger.info("Consumer - Starting...")
+    client.subscribe_to_topic(topic_name=topic_name,
+                              consumer_group_prefix=consumer_group)
+
+    for message in client.messages():
+        logger.info(message)
+        content = message.value.decode("utf-8")
+        origin_id, sequence_number = content.split("-")
+        content = {"_id": uuid.uuid4().hex, "content": content,
+                   "origin_id": origin_id, "sequence_number": sequence_number,
+                   "destination": process_id, "consumer_group": consumer_group}
+        if collection is not None:
+            try:
+                collection.insert_one(content)
+            except DuplicateKeyError:
+                logger.error(f"Duplicated key with id: {content['_id']}")
+
+
+if __name__ == "__main__":
+    app()
